8468714ab5ffa25e99a1a7174559e34454ec5a5d
francd <fcamadi@gmail.com>
2022-05-18 23:34:07 +0200

    test: add deposit 100 in 20220505

--- src/test/java/kata/AccountServiceShould.java
+++ src/test/java/kata/AccountServiceShould.java
@@ -1,14 +1,48 @@
 package kata;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.time.LocalDate;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceShould {
 
+
+    @Spy
+    TransactionRepository transactionRepository;
+
+    @Mock
+    ClockGenerator clockGenerator;
+
+    private AccountService accountService;
+
     @Test
-    void test() {
-        assertThat(true).isTrue();
+    void add_deposit_100_in_20220505() {
+
+        //given
+        int amount = 10;
+        LocalDate date = LocalDate.of(2022, 5, 5);
+        //when(clockGenerator.getDate()).thenReturn(date);
+        given(clockGenerator.getDate()).thenReturn(date);  //same as "when"!
+        accountService = new AccountService();
+
+
+        //when
+        accountService.deposit(amount);
+
+
+        //verify
+        Transaction transaction = new Transaction(amount, date);
+        verify(transactionRepository, times(1)).add(transaction);
     }
 
 }
