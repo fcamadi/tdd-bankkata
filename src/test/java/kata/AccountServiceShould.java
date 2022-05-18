dba0cbd01c618d66a8520d3f25373b96b6538d8f
francd <fcamadi@gmail.com>
2022-05-18 22:50:26 +0200

    initial commit (project creation)

---
+++ src/test/java/kata/AccountServiceShould.java
@@ -0,0 +1,14 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountServiceShould {
+
+    @Test
+    void test() {
+        assertThat(true).isTrue();
+    }
+
+}
